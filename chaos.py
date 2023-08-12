6ea75280856a88278ef918ee50f8395c0655bf90
Stevecool023 <tallmankaranjastevee@gmail.com>
2023-08-12 08:06:50 -0700

    chaotic behavior

---
+++ chaos.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+# This program illustrates chaotic behavior.
+# It is inspired by the form: k(x)(x-1)
+# + and is widely employed by physicists and in the field of mathematics.
+
+def main():
+    print('A simple program illustrating chaotic behavior')
+    x = eval(input('Enter a number between 0 and 1: '))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        print(x)
+
+main()
