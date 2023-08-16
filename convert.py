1d20408ab893286c448ac9c68bf29bfe1cd45b0b
Stevecool023 <tallmankaranjastevee@gmail.com>
2023-08-16 13:13:35 -0700

    converting celsius to fahreheit

---
+++ convert.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+# This progran converts temperature in Fahrenheit
+# +to degrees celsius.
+# Mathematical formulae: F = 9/5C + 32.
+
+def main():
+    print("convert degrees celsius to degrees fahrenheit")
+    # Prompt user to input temp value in C.
+    celsius = eval(input("Enter temp in degrees celsius: "))
+    # conver celsius to F.
+    F = 9 / 5 * celsius + 32
+    print("{} degrees celsius equals "
+    "{:.2f} degrees Fahrenheit".format(celsius, F))
+
+main()
