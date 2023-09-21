d3b60b96d93eed9db8958d6dd625b0a4c5e3fcd4
Stevecool023 <karanjastephenn@gmail.com>
2023-09-21 02:09:05 -0700

    sum of entered numbers

--- number_series.py
+++ number_series.py
@@ -10,7 +10,7 @@ print('Sum of series of numbers entered by the user.')
 n = int(input('Enter count of numbers to be summed: '))
 total = 0
 for i in range(n):
-    i = float(input(f'{i+1} : ',))
+    i = float(input(f'{i+1} : '))
     total = total + i
 print('Sum of the above', n, 'numbers is ', total)
 print()
